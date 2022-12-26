bda2f2e64de5613ee18c39a66bd9cd93c62ec2aa
cjw4458 <cjw1234577@gmail.com>
2022-12-26 22:14:27 +0900

    feat: MainPage 내비게이션 바 버튼 추가

--- lib/pages/MainPage.dart
+++ lib/pages/MainPage.dart
@@ -9,15 +9,90 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  int _currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () async {
-            await FirebaseAuth.instance.signOut();
-          }, child: Text("로그아웃"),
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            icon: const Icon(
+              Icons.arrow_back_ios,
+              color: Color(0xff5f66f2),
+            ),
+            onPressed: () async => await FirebaseAuth.instance.signOut(),
+          ),
+        ],
+        shape: Border(bottom: BorderSide(color: Color(0xffdbdbdb), width: 2.5)),
+        backgroundColor: Color(0xfffafafa),
+        elevation: 0,
+        title: Text(
+          'CONNEC',
+          style: TextStyle(
+            color: Color(0xff5f66f2),
+            fontSize: 25,
+            fontWeight: FontWeight.w900,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: Container(),
+      bottomNavigationBar: Container(
+        height: 70,
+        child: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (index) {
+            print('index test : ${index}');
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Color(0xff5f66f2),
+          showSelectedLabels: true,
+          fixedColor: Colors.white,
+          unselectedItemColor: Colors.white,
+          selectedFontSize: 12,
+          unselectedFontSize: 12,
+          items: [
+            BottomNavigationBarItem(
+                icon: Image.asset(
+                  "assets/images/navigation_icon_1.png",
+                  height: 30,
+                  width: 30,
+                ),
+
+                label: "네트워크"),
+            BottomNavigationBarItem(
+                icon: Image.asset(
+                  "assets/images/navigation_icon_2.png",
+                  height: 30,
+                  width: 30,
+                ),
+                label: "지인관리"),
+            BottomNavigationBarItem(
+                icon: Image.asset(
+                  "assets/images/navigation_icon_3.png",
+                  height: 30,
+                  width: 30,
+                ),
+                label: "검색"),
+            BottomNavigationBarItem(
+                icon: Image.asset(
+                  "assets/images/navigation_icon_4.png",
+                  height: 30,
+                  width: 30,
+                ),
+                label: "구인장터"),
+            BottomNavigationBarItem(
+                icon: Image.asset(
+                  "assets/images/navigation_icon_5.png",
+                  height: 30,
+                  width: 30,
+                ),
+                label: "마이페이지"),
+          ],
         ),
       ),
     );
