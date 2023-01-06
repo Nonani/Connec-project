33baff03b0fa172a7926ba287cacfac7a410b9bb
cjw4458 <cjw1234577@gmail.com>
2023-01-06 22:23:19 +0900

    feat:json to map 변환 기능 구현

--- lib/components/custom_expansion_tile.dart
+++ lib/components/custom_expansion_tile.dart
@@ -1,8 +1,5 @@
-// Copyright 2017 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 import 'package:flutter/material.dart';
+import 'dart:convert';
 
 class ExpansionTileSample extends StatelessWidget {
   @override
@@ -16,88 +13,68 @@ class ExpansionTileSample extends StatelessWidget {
 }
 
 // One entry in the multilevel list displayed by this app.
-class Entry {
-  Entry(this.title, [this.children = const <Entry>[]]);
+List<Entry> parseEntries(String jsonString) {
+  final List<dynamic> parsed = jsonDecode(jsonString);
+  List<Entry> entries = [];
+  for (var entry in parsed) {
+    entries.add(Entry.fromJson(entry));
+  }
+  return entries;
+}
 
+class Entry {
   final String title;
   final List<Entry> children;
 
+  Entry({required this.title, required this.children});
+
+  factory Entry.fromJson(Map<String, dynamic> json) {
+    var childrenJson = json['children'] as List<dynamic>;
+    List<Entry> children = childrenJson.map((i) => Entry.fromJson(i)).toList();
+    return Entry(
+      title: json['title'],
+      children: children,
+    );
+  }
 }
 
-List<Entry> jsonToEntry(dynamic json){
-  List<Entry> data = List<Entry>.empty();
-  return data;
-}
+String json = """
+[
+  {
+    "title": "String1",
+    "children": [
+      { "title": "1", "children": [
+      {"title" : "1", "children" : []
+      }
+      ] },
+      { "title": "2", "children": [] }
+    ]
+  },
+  {
+    "title": "String2",
+    "children": [
+      { "title": "3", "children": [] },
+      { "title": "4", "children": [] }
+    ]
+  }]
+""";
 
 // The entire multilevel list displayed by this app.
-final List<Entry> data = <Entry>[
-  Entry(
-    '레슨',
-    <Entry>[
-      Entry(
-        'Section A0',
-        <Entry>[
-          Entry('Item A0.1'),
-          Entry('Item A0.2'),
-          Entry('Item A0.3'),
-        ],
-      ),
-      Entry('Section A1'),
-      Entry('Section A2'),
-    ],
-  ),
-  Entry(
-    '홈/리빙',
-    <Entry>[
-      Entry('Section B0'),
-      Entry('Section B1'),
-    ],
-  ),
-  Entry(
-    '이벤트',
-    <Entry>[
-      Entry('Section C0'),
-      Entry('Section C1'),
-      Entry(
-        'Section C2',
-        <Entry>[
-          Entry('Item C2.0'),
-          Entry('Item C2.1'),
-          Entry('Item C2.2'),
-          Entry('Item C2.3'),
-        ],
-      ),
-    ],
-  ),
-  Entry(
-    '비즈니스',
-    <Entry>[
-      Entry('Section C0'),
-      Entry('Section C1'),
-      Entry(
-        'Section C2',
-        <Entry>[
-          Entry('Item C2.0'),
-          Entry('Item C2.1'),
-          Entry('Item C2.2'),
-          Entry('Item C2.3'),
-        ],
-      ),
-    ],
-  ),
-];
+final List<Entry> data = parseEntries(json);
 
 // Displays one Entry. If the entry has children then it's displayed
 // with an ExpansionTile.
 class EntryItem extends StatelessWidget {
   const EntryItem(this.entry, this.num);
+
   final double num;
   final Entry entry;
 
   Widget _buildTiles1(Entry root) {
     if (root.children.isEmpty)
       return ListTile(
-        title: Text(root.title,
+        title: Text(
+          root.title,
           style: TextStyle(
             color: Color(0xff666666),
             fontFamily: 'EchoDream',
@@ -108,17 +85,16 @@ class EntryItem extends StatelessWidget {
           // 이 부분을 go_router 패키지 써서
           // context.go('/${root.title}'), 이런 식으로 할 예정
           print(root.title);
-          print(1);
         },
       );
     return Container(
       decoration: UnderlineTabIndicator(
-        borderSide: BorderSide(width: 1, color: Color(0xff5f66f2))
-      ),
+          borderSide: BorderSide(width: 1, color: Color(0xff5f66f2))),
       child: ExpansionTile(
         childrenPadding: EdgeInsets.only(left: 10),
         key: PageStorageKey<Entry>(root),
-        title: Text(root.title,
+        title: Text(
+          root.title,
           style: TextStyle(
             color: Color(0xff666666),
             fontSize: 16,
@@ -130,10 +106,12 @@ class EntryItem extends StatelessWidget {
       ),
     );
   }
+
   Widget _buildTiles2(Entry root) {
     if (root.children.isEmpty)
       return ListTile(
-        title: Text(root.title,
+        title: Text(
+          root.title,
           style: TextStyle(
             color: Color(0xff666666),
             fontFamily: 'EchoDream',
@@ -144,14 +122,14 @@ class EntryItem extends StatelessWidget {
           // 이 부분을 go_router 패키지 써서
           // context.go('/${root.title}'), 이런 식으로 할 예정
           print(root.title);
-          print(2);
         },
       );
     return Container(
       child: ExpansionTile(
         childrenPadding: EdgeInsets.only(left: 10),
         key: PageStorageKey<Entry>(root),
-        title: Text(root.title,
+        title: Text(
+          root.title,
           style: TextStyle(
             color: Color(0xff666666),
             fontFamily: 'EchoDream',
@@ -162,9 +140,9 @@ class EntryItem extends StatelessWidget {
       ),
     );
   }
+
   @override
   Widget build(BuildContext context) {
     return _buildTiles1(entry);
   }
 }
-
