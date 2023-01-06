daf4b1bf2c5229754aff0186594bf1377a07096d
signo <ukleon123@gmail.com>
2023-01-06 20:59:49 +0900

    implement network information page

--- lib/pages/network_information_page.dart
+++ lib/pages/network_information_page.dart
@@ -55,169 +55,171 @@ class _NetworkInformationPageState extends State<NetworkInformationPage> {
             );
           } else {
             return Scaffold(
-                appBar: AppBar(
-                  elevation: 0,
-                  toolbarHeight: 56,
-                  backgroundColor: const Color(0xfffafafa),
-                  leading: IconButton(
-                    icon: const Icon(
-                      Icons.arrow_back_ios,
-                      color: Color(0xff5f66f2),
-                    ),
-                    onPressed: () => Navigator.of(context).pop(),
+              appBar: AppBar(
+                elevation: 0,
+                toolbarHeight: 56,
+                backgroundColor: Colors.transparent,
+                leading: IconButton(
+                  icon: const Icon(
+                    Icons.arrow_back_ios,
+                    color: Color(0xff5f66f2),
                   ),
+                  onPressed: () => Navigator.of(context).pop(),
                 ),
-                body: Column(
-                    children: [
+              ),
+              extendBodyBehindAppBar: true,
+              body: Center(
+                child: Column(children: [
+                  Container(
+                      width: double.infinity,
+                      height: 250,
+                      child: Image.network("")),
                   Container(
                       height: 450,
                       width: 330,
-                      margin: const EdgeInsets.only(top: 27, bottom: 9),
-                      child:
-                          SingleChildScrollView(
-                            padding: EdgeInsets.only(left: 12.5, right: 11.5),
-                              child: Center(
-                                child: Column(
-                                    children: [
-                                      Column(),
-                                      Padding(
-                                        padding: const EdgeInsets.only(left:17.5, right: 17.5),
-                                        child: Row(
-
-                                          mainAxisAlignment:
-                                        MainAxisAlignment
-                                            .spaceBetween,
-                                          children: [
-                                            Column(
-                                              crossAxisAlignment:
-                                              CrossAxisAlignment
-                                                  .start,
-                                              children: [
-                                                Text("지인 평점",
-                                                    style: _contextStyle),
-                                                Text("지인 수",
-                                                    style: _contextStyle),
-                                                Text("지인 대표 분야",
-                                                    style: _contextStyle)
-                                              ],
-                                            ),
-                                            Column(
-                                              crossAxisAlignment:
-                                              CrossAxisAlignment.end,
-                                              children: [
-                                                Text("/5.0",
-                                                    style: _contextStyle),
-                                                Text("",
-                                                    style: _contextStyle),
-                                                Text("",
-                                                    style: _contextStyle)
-                                              ],
-                                            )
-                                          ],),
-                                      ),
-                                      Padding(
-                                        padding: const EdgeInsets.only(left:17.5, right: 18.5),
-                                        child: Row(mainAxisAlignment:
-                                        MainAxisAlignment
-                                            .spaceBetween,
-                                          children: [
-                                            Column(
-                                              crossAxisAlignment:
-                                              CrossAxisAlignment
-                                                  .start,
-                                              children: [
-                                                Text("지인 평점",
-                                                    style: _contextStyle),
-                                                Text("지인 수",
-                                                    style: _contextStyle),
-                                                Text("지인 대표 분야",
-                                                    style: _contextStyle)
-                                              ],
-                                            ),
-                                            Column(
-                                              crossAxisAlignment:
-                                              CrossAxisAlignment.end,
-                                              children: [
-                                                Text("/5.0",
-                                                    style: _contextStyle),
-                                                Text("",
-                                                    style: _contextStyle),
-                                                Text("",
-                                                    style: _contextStyle)
-                                              ],
-                                            )
-                                          ],),
-                                      )
-                                      ]
+                      margin: const EdgeInsets.only(top: 13, bottom: 10.5),
+                      child: SingleChildScrollView(
+                          child: Column(children: [
+                        Container(
+                          padding: EdgeInsets.only(top: 13, bottom: 13),
+                          child: const Text(
+                            '김컨넥',
+                            style: TextStyle(
+                              color: Color(0xff333333),
+                              fontSize: 21,
+                              fontFamily: 'EchoDream',
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          width: 336,
+                          height: 0,
+                          margin: const EdgeInsets.only(top: 7.5, bottom: 10.5),
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Color(0xff5f66f2),
+                              width: 1,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          padding:
+                              const EdgeInsets.only(left: 17.5, right: 17.5),
+                          child: Column(
+                            children: [
+                              Container(
+                                padding: EdgeInsets.only(bottom: 13),
+                                child: const Text(
+                                  '네트워크 정보',
+                                  style: TextStyle(
+                                    color: Color(0xff333333),
+                                    fontSize: 16,
+                                    fontFamily: 'EchoDream',
+                                    fontWeight: FontWeight.w500,
+                                  ),
                                 ),
-                              )
-                          )
-                  ),
-
+                              ),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text("지인 평점", style: _contextStyle),
+                                      Text("지인 수", style: _contextStyle),
+                                      Text("지인 대표 분야", style: _contextStyle)
+                                    ],
+                                  ),
+                                  Column(
+                                    crossAxisAlignment: CrossAxisAlignment.end,
+                                    children: [
+                                      Text("/5.0", style: _contextStyle),
+                                      Text("", style: _contextStyle),
+                                      Text("", style: _contextStyle)
+                                    ],
+                                  )
+                                ],
+                              ),
+                              Container(
+                                width: 336,
+                                height: 0,
+                                margin: const EdgeInsets.only(
+                                    top: 7.5, bottom: 10.5),
+                                decoration: BoxDecoration(
+                                  border: Border.all(
+                                    color: Color(0xffdbdbdb),
+                                    width: 1,
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                padding: EdgeInsets.only(bottom: 13),
+                                child: const Text(
+                                  '개인정보',
+                                  style: TextStyle(
+                                    color: Color(0xff333333),
+                                    fontSize: 16,
+                                    fontFamily: 'EchoDream',
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                              ),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text("이름", style: _contextStyle),
+                                      Text("직군/직무", style: _contextStyle),
+                                      Text("경력", style: _contextStyle),
+                                      Text("활동지", style: _contextStyle),
+                                      Text("성별", style: _contextStyle),
+                                      Text("나이", style: _contextStyle),
+                                      Text("능력", style: _contextStyle),
+                                    ],
+                                  ),
+                                  Column(
+                                    crossAxisAlignment: CrossAxisAlignment.end,
+                                    children: [
+                                      Text("", style: _contextStyle),
+                                      Text("", style: _contextStyle),
+                                      Text("", style: _contextStyle),
+                                      Text("", style: _contextStyle),
+                                      Text("", style: _contextStyle),
+                                      Text("", style: _contextStyle),
+                                      Text("", style: _contextStyle),
+                                    ],
+                                  )
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ]))),
                 ]),
-                bottomNavigationBar: Container(
-                  height: 70,
-                  child: BottomNavigationBar(
-                    currentIndex: _currentIndex,
-                    onTap: (index) {
-                      print('index test : ${index}');
-                      if (_currentIndex != index) {
-                        setState(() {
-                          _currentIndex = index;
-                        });
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => list[_currentIndex],
-                            ));
-                      }
-                    },
-                    type: BottomNavigationBarType.fixed,
-                    backgroundColor: Color(0xff5f66f2),
-                    showSelectedLabels: true,
-                    fixedColor: Colors.white,
-                    unselectedItemColor: Colors.white,
-                    selectedFontSize: 12,
-                    unselectedFontSize: 12,
-                    items: [
-                      BottomNavigationBarItem(
-                          icon: Image.asset(
-                            "assets/images/navigation_icon_1.png",
-                            height: 30,
-                            width: 30,
-                          ),
-                          label: "네트워크"),
-                      BottomNavigationBarItem(
-                          icon: Image.asset(
-                            "assets/images/navigation_icon_2.png",
-                            height: 30,
-                            width: 30,
-                          ),
-                          label: "지인관리"),
-                      BottomNavigationBarItem(
-                          icon: Image.asset(
-                            "assets/images/navigation_icon_3.png",
-                            height: 30,
-                            width: 30,
-                          ),
-                          label: "검색"),
-                      BottomNavigationBarItem(
-                          icon: Image.asset(
-                            "assets/images/navigation_icon_4.png",
-                            height: 30,
-                            width: 30,
-                          ),
-                          label: "구인장터"),
-                      BottomNavigationBarItem(
-                          icon: Image.asset(
-                            "assets/images/navigation_icon_5.png",
-                            height: 30,
-                            width: 30,
-                          ),
-                          label: "마이페이지"),
-                    ],
+              ),
+              bottomNavigationBar: ElevatedButton(
+                onPressed: () async {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xff5f66f2),
+                  minimumSize: const Size(100, 56),
+                ),
+                child: const Text(
+                  '삭제하기',
+                  style: TextStyle(
+                    color: Color(0xfffafafa),
+                    fontSize: 20,
+                    fontFamily: 'EchoDream',
                   ),
-                ));
+                ),
+              ),
+            );
           }
         });
   }
