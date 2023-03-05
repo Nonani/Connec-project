c57938ba4633315d8811df81f83dd4c4efcb713b
cjw4458 <cjw1234577@gmail.com>
2023-03-05 13:34:25 +0900

    feat: 로그인 페이지 수정

--- lib/pages/login/login_page.dart
+++ lib/pages/login/login_page.dart
@@ -38,175 +38,62 @@ class _LoginPageState extends State<LoginPage> {
         shape: Border(bottom: BorderSide(color: Color(0xffdbdbdb), width: 2.5)),
         backgroundColor: Color(0xfffafafa),
         elevation: 0,
-        title: Text("로그인",
-            style: featureTitle
-        ),
+        title: Text("로그인", style: featureTitle),
         centerTitle: true,
       ),
       body: SafeArea(
         child: ListView(
           children: [
-            SizedBox(
-              height: size.height * 0.1,
-            ),
             Container(
+              margin: EdgeInsets.only(top: size.height * 0.18),
               width: double.infinity,
-              height: size.height * 0.15,
+              height: size.height * 0.072,
               child: Center(
                 child: Text(
-                  "CONNEC",
+                  'CONNEC',
                   style: TextStyle(
-                      fontSize: 40,
-                      fontFamily: "EchoDream",
-                      fontWeight: FontWeight.w800,
-                      color: Color(0xff5f66f2)),
+                    color: Color(0xff5f66f2),
+                    fontSize: 40,
+                    fontFamily: 'S-CoreDream-8',
+                    fontWeight: FontWeight.w900,
+                  ),
                 ),
               ),
             ),
-            Form(
-              key: _formKey,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  LoginEditTextForm(
-                    label: "이메일",
-                    hint: "example@connec.co.kr",
-                    isSecret: false,
-                    onSaved: (newValue) {
-                      email = newValue;
-                      print(email);
-                    },
-                  ),
-                  LoginEditTextForm(
-                    label: "비밀번호",
-                    hint: "비밀번호를 입력해주세요",
-                    isSecret: true,
-                    onSaved: (newValue) {
-                      password = newValue;
-                      print(password);
-                    },
-                  ),
-                  Container(
-                    padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
-                    width: double.infinity,
-                    height: 70,
-                    child: ElevatedButton(
-                        onPressed: () async {
-                          if (_formKey.currentState!.validate()) {
-                            _formKey.currentState!.save();
-                            try {
-                              final credential = await FirebaseAuth.instance
-                                  .signInWithEmailAndPassword(
-                                      email: email, password: password);
-                              getToken();
-                            } on FirebaseAuthException catch (e) {
-                              if (e.code == 'user-not-found') {
-                                logger.w('No user found for that email.');
-                              } else if (e.code == 'wrong-password') {
-                                logger.w(
-                                    'Wrong password provided for that user.');
-                              } else {
-                                logger.w(e);
-                              }
-                            }
-                          }
-                        },
-                        child: Text(
-                          "이메일 로그인",
-                          style: TextStyle(
-                              fontFamily: "EchoDream",
-                              fontWeight: FontWeight.w500,
-                              fontSize: 16),
-                        ),
-                        style: ButtonStyle(
-                            backgroundColor: MaterialStateProperty.all(
-                              Color(0xff5f66f2),
-                            ),
-                            shape: MaterialStateProperty.all<
-                                RoundedRectangleBorder>(RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(25.0),
-                            )))),
-                  ),
-                ],
+            SizedBox(height: size.height * 0.34),
+            Center(
+              child: Text(
+                '카카오톡으로 로그인하기',
+                style: TextStyle(
+                  color: Color(0xff5f66f2),
+                  fontSize: 17,
+                  fontFamily: 'S-CoreDream-6',
+                  fontWeight: FontWeight.w700,
+                ),
               ),
             ),
-            Row(
+            SizedBox(height: size.height * 0.05),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                TextButton(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => ResetPasswordPage(),
-                          ));
-                    },
-                    child: Text(
-                      "비밀번호 찾기",
-                      style: TextStyle(color: Color(0xffbdbdbd)),
-                    )),
-                SizedBox(
-                  height: size.height * 0.01,
-                  child: VerticalDivider(
-                    color: Color(0xffbdbdbd),
-                    //color of divider
-                    width: 10,
-                    //width space of divider
-                    thickness: 1,
-                    //thickness of divier line
-                    indent: 10,
-                    //Spacing at the top of divider.
-                    endIndent: 10, //Spacing at the bottom of divider.
-                  ),
-                ),
-                TextButton(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => SignUpPage(),
-                          ));
-                    },
-                    child: Text(
-                      "회원가입",
-                      style: TextStyle(color: Color(0xffbdbdbd)),
-                    )),
-              ],
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Center(
-                child: Text(
-              "다른 방법으로 로그인 하기",
-              style: TextStyle(
-                fontSize: 13,
-                fontWeight: FontWeight.w500,
-              ),
-            )),
-            SizedBox(height: size.height * 0.02),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                SizedBox(width: size.width * 0.01),
                 customImageButton(AssetImage("assets/images/kakao_btn.png"),
                     () async {
-                  // _showDialog();
                   await loginWithKakao(context);
-                  // Navigator.pop(context);
                 }, 70),
-                customImageButton(AssetImage("assets/images/naver_btn.png"),
-                    () {
-                  print("clicked!!");
-                }, 70),
-                customImageButton(AssetImage("assets/images/facebook_btn.png"),
-                    () {
-                  print("clicked!!");
-                }, 70),
-                SizedBox(width: size.width * 0.01),
+                SizedBox(height: size.height * 0.03,),
+                Text(
+                  '아이콘을 눌러\n카카오톡과 연결해주세요',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Color(0xffbdbdbd),
+                    fontSize: 11.5,
+                    fontFamily: 'S-CoreDream-4',
+                  ),
+                ),
               ],
-            )
+            ),
+
           ],
         ),
       ),
