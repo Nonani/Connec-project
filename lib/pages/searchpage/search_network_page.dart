54753e0b08461df5e29d5ec2555d09174853ae0c
signo <ukleon123@gmail.com>
2023-03-18 15:22:35 +0900

    Feature search with relation degree

--- lib/pages/searchpage/search_network_page.dart
+++ lib/pages/searchpage/search_network_page.dart
@@ -4,12 +4,14 @@ import 'package:connec/pages/searchpage/search_network_detail_page.dart';
 import 'package:connec/style/titlestyle.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
 
 import '../../components/member_item_widget.dart';
 
 class SearchNetworkPage extends StatelessWidget {
-  const SearchNetworkPage(this.code, {Key? key}) : super(key: key);
+  SearchNetworkPage(this.code, {Key? key}) : super(key: key);
   final String code;
+  final FirebaseFirestore db = FirebaseFirestore.instance;
   final TextStyle _nameStyle = const TextStyle(
     color: Color(0xff333333),
     fontSize: 15,
@@ -99,7 +101,7 @@ class SearchNetworkPage extends StatelessWidget {
                                         mainField: snapshot.data['list'][index]
                                             ['bTitle'],
                                         subField: snapshot.data['list'][index]
-                                        ['lTitle'],
+                                            ['lTitle'],
                                         // field: snapshot.data['list'][index]['work'],
                                         rate: '0',
                                         relationship: '한 다리',
@@ -121,69 +123,101 @@ class SearchNetworkPage extends StatelessWidget {
   }
 
   Future _future() async {
-    final db = FirebaseFirestore.instance;
-
-    var user = await db
-        .collection("users")
-        .where("workArea", arrayContains: code)
-        .get();
-    var member = await db
-        .collection("member")
-        .where("workArea", arrayContains: code)
-        .get();
+    final logger = Logger();
     Map<String, dynamic> data = {};
-    data['list'] = List.empty(growable: true);
 
-    int user_cnt = 0, member_cnt = 0;
-    for (var element in user.docs) {
-      var elementData = element.data();
-      QuerySnapshot<Map<String, dynamic>> tmp = await db
-          .collection('workData')
-          .where('code', isEqualTo: elementData['workArea'][0])
-          .get();
-      elementData['lTitle'] = tmp.docs[0]['title'];
-      tmp = await db
-          .collection('workData')
-          .where('code', isEqualTo: '${elementData['workArea'][0][0]}00')
-          .get();
-      elementData['bTitle'] = tmp.docs[0]['title'];
-      tmp = await db
-          .collection('localData')
-          .where('code', isEqualTo: elementData['location'])
-          .get();
-      elementData['location'] = tmp.docs[0]['title'];
+    data['list'] = [];
+    data['length'] = 0;
+    List<dynamic> firstUser = [];
+    List<dynamic> secondUser = [];
+    List<dynamic> thirdUser = [];
 
-      if (element["uid"] != FirebaseAuth.instance.currentUser!.uid) {
-        data['list'].add(elementData);
-        user_cnt++;
+    //한다리
+    try {
+      firstUser = (await db
+              .collection('networks')
+              .doc(FirebaseAuth.instance.currentUser!.uid)
+              .get())
+          .data()!['list'];
+
+      for (String user in firstUser) {
+        Map<String, dynamic>? tmp =
+            (await db.collection('users').doc(user).get()).data();
+        if (tmp!['workArea'].contains(code)) {
+          tmp = await getWork(tmp);
+          data['list'].add(tmp);
+        }
       }
+    } catch (e) {
+      logger.w(e);
     }
 
-    for (var element in member.docs) {
-      var elementData = element.data();
-      QuerySnapshot<Map<String, dynamic>> tmp = await db
-          .collection('workData')
-          .where('code', isEqualTo: elementData['workArea'][0])
-          .get();
-      elementData['lTitle'] = tmp.docs[0]['title'];
-      tmp = await db
-          .collection('workData')
-          .where('code', isEqualTo: '${elementData['workArea'][0][0]}00')
-          .get();
-      elementData['bTitle'] = tmp.docs[0]['title'];
-      tmp = await db
-          .collection('localData')
-          .where('code', isEqualTo: elementData['location'])
-          .get();
-      elementData['location'] = tmp.docs[0]['title'];
+    //두다리
+    try {
+      for (var users in (await db
+              .collection('networks')
+              .where(FieldPath.documentId, whereIn: firstUser)
+              .get())
+          .docs) {
+        for (var user in users.data()['list']) {
+          if (!firstUser.contains(user)) {
+            secondUser.add(user);
+          }
+        }
+      }
+      for (String user in secondUser) {
+        Map<String, dynamic>? tmp =
+            (await db.collection('users').doc(user).get()).data();
+        if (tmp!['workArea'].contains(code)) {
+          tmp = await getWork(tmp);
+          data['list'].add(tmp);
+        }
+      }
+    } catch (e) {
+      logger.w(e);
+    }
 
-      if (element["uid"] != FirebaseAuth.instance.currentUser!.uid) {
-        data['list'].add(elementData);
-        member_cnt++;
+    //세다리
+    try {
+      for (var users in (await db
+              .collection('networks')
+              .where(FieldPath.documentId, whereIn: secondUser)
+              .get())
+          .docs) {
+        for (var user in users.data()['list']) {
+          if (!firstUser.contains(user) && !secondUser.contains(user)) {
+            thirdUser.add(user);
+          }
+        }
+      }
+      for (String user in thirdUser) {
+        Map<String, dynamic>? tmp =
+            (await db.collection('users').doc(user).get()).data();
+        if (tmp!['workArea'].contains(code)) {
+          tmp = await getWork(tmp);
+          data['list'].add(tmp);
+        }
+      }
+    } catch (e) {
+      logger.w(e);
+    }
+
+    data['length'] = data['list'].length;
+    return data;
+  }
+
+  Future<Map<String, dynamic>> getWork(Map<String, dynamic> data) async {
+    List<QueryDocumentSnapshot<Map<String, dynamic>>> work = (await db
+            .collection('workData')
+            .where(FieldPath.documentId, whereIn: [code, "${code[0]}00"]).get())
+        .docs;
+    for (QueryDocumentSnapshot<Map<String, dynamic>> element in work) {
+      if (element['code'] == "${code[0]}00") {
+        data['bTitle'] = element.data()['title'];
+      } else {
+        data['lTitle'] = element.data()['title'];
       }
     }
-    data['length'] = user_cnt + member_cnt;
-
     return data;
   }
 }
